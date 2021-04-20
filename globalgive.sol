267c8a082ca156d7b790b297c507f02b2a188b89
Global Give <femigdev@gmail.com>
2021-04-20 16:12:13 -0400

    My first commit

--- globalgive.sol
+++ globalgive.sol
@@ -12,11 +12,11 @@
 
    #LIQ+#RFI+#SHIB+#DOGE = #BEE
 
-   #SAFEMOON features:
-   3% fee auto add to the liquidity pool to locked forever when selling
+   #GG features:
+   5% fee auto add to the liquidity pool to locked for 4 years
    2% fee auto distribute to all holders
    I created a black hole so #Bee token will deflate itself in supply with every transaction
-   50% Supply is burned at start.
+
 
 
  */
